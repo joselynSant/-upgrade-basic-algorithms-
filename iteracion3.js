c7a0485cfe0a94947116b3dd710da01b2ad3e203
joselyn santoveña <94395924+joselynSant@users.noreply.github.com>
2021-12-08 22:34:55 +0100

    Add files via upload

---
+++ iteracion3.js
@@ -0,0 +1,31 @@
+//Iteración #3: Operadores**
+
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+let num1 = 10
+let num2 = 5
+alert(num1 * num2)
+
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+let numa = 10
+let numb = 2
+alert(numa / numb)
+
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+let numero = 15
+numero %= 9
+alert(numero)
+
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+//teniendo dos variables y = 10 y z = 5.
+let y = 10
+let z = 5
+let x = (y += z)
+document.write(x)
+
+//1.5 Usa el correcto operador de asignación que resultará en x = 50,
+//teniendo dos variables y = 10 y z = 5.
+let y = 10
+let z = 5
+let x = (y *= z)
+document.write(x)
